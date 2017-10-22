aa65e6471719564d78cc35148773c66b24bf2ffd
mrgreywater <mr.greywater@googlemail.com>
2017-10-22 22:05:33 +0200

    viz: add antialiasing, reduce zoom artifacts

--- test/viz.cpp
+++ test/viz.cpp
@@ -17,18 +17,88 @@
 static GLFWwindow *window = nullptr;
 static const int width = 1024;
 static const int height = 1024;
-static int fbWidth = 0;
-static int fbHeight = 0;
 static bool drawFill = true;
 static bool drawMesh = true;
 static bool dirtyViewport = true, dirtyShape = true, dirtyTessellator = true;
 
-static double left = 0, right = 0, bottom = 0, top = 0;
 static bool mouseDrag = false;
 
 static std::size_t shapeIndex = 0;
-
 static std::size_t tessellator = 0;
+
+struct Camera2D {
+    double left = .0, right = .0, bottom = .0, top = .0;
+    double translateX = .0, translateY = .0;
+    int viewWidth = 0, viewHeight = 0;
+    double zoom = -1.;
+    double mx = .0, cx = .0, my = .0, cy = .0;
+    inline double scaling() { return std::pow(1.1, zoom); }
+    void setView(int width, int height) {
+        viewWidth = width;
+        viewHeight = height;
+    }
+    void limits(double l, double r, double b, double t) {
+        left = l;
+        right = r;
+        bottom = b;
+        top = t;
+    }
+    bool scale(double z) {
+        if (z == 0) return false;
+        zoom += z;
+        return true;
+    }
+    bool move(double x, double y) {
+        const double s = scaling();
+        const double dx = x / double(viewWidth) * (right - left) / s;
+        const double dy = y / double(viewHeight) * (bottom - top) / s;
+        if (dx == 0 && dy == 0) {
+            return false;
+        }
+        translateX += dx;
+        translateY += dy;
+        return true;
+    }
+    /* apply current transform to opengl context */
+    void apply() {
+        glViewport(0, 0, viewWidth, viewHeight);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        const double s = scaling();
+        const double w2 = .5 * (right - left);
+        const double h2 = .5 * (top - bottom);
+        mx = s / w2;
+        cx = (-left - w2 + translateX) * mx;
+        my = s / h2;
+        cy = (-bottom - h2 + translateY) * my;
+    }
+    void setDefaults() {
+        zoom = -1.;
+        translateX = .0;
+        translateY = .0;
+    }
+    /* transforms world coordinate to screen range [-1,1] with double precision */
+    inline void toScreen(double *x, double *y) {
+        *x = *x * mx + cx;
+        *y = *y * my + cy;
+    }
+    /* transforms screen coordinates in range [-1,1] to world coordinates with double precision */
+    inline void toWorld(double *x, double *y) {
+        *x = (*x - cx) / mx;
+        *y = (*y - cy) / my;
+    }
+    inline void vec2(double x, double y) {
+        toScreen(&x, &y);
+        glVertex2d(x, y);
+    }
+};
+
+static Camera2D cam;
 
 class DrawablePolygon {
 public:
@@ -47,16 +117,16 @@ public:
     void drawMesh() override {
         const auto &v = shape.vertices;
         const auto &x = shape.indices;
-        glLineWidth(float(fbHeight) / height);
+        glLineWidth(float(cam.viewHeight) / height);
         glBegin(GL_LINES);
         glColor3f(1, 0, 0);
         for (size_t i = 0; i < x.size(); i += 3) {
-            glVertex2f(static_cast<GLfloat>(v[x[i]][0]), static_cast<GLfloat>(v[x[i]][1]));
-            glVertex2f(static_cast<GLfloat>(v[x[i + 1]][0]), static_cast<GLfloat>(v[x[i + 1]][1]));
-            glVertex2f(static_cast<GLfloat>(v[x[i + 1]][0]), static_cast<GLfloat>(v[x[i + 1]][1]));
-            glVertex2f(static_cast<GLfloat>(v[x[i + 2]][0]), static_cast<GLfloat>(v[x[i + 2]][1]));
-            glVertex2f(static_cast<GLfloat>(v[x[i + 2]][0]), static_cast<GLfloat>(v[x[i + 2]][1]));
-            glVertex2f(static_cast<GLfloat>(v[x[i]][0]), static_cast<GLfloat>(v[x[i]][1]));
+            cam.vec2(v[x[i]][0], v[x[i]][1]);
+            cam.vec2(v[x[i + 1]][0], v[x[i + 1]][1]);
+            cam.vec2(v[x[i + 1]][0], v[x[i + 1]][1]);
+            cam.vec2(v[x[i + 2]][0], v[x[i + 2]][1]);
+            cam.vec2(v[x[i + 2]][0], v[x[i + 2]][1]);
+            cam.vec2(v[x[i]][0], v[x[i]][1]);
         }
         glEnd();
     };
@@ -66,7 +136,7 @@ public:
         glBegin(GL_TRIANGLES);
         glColor3f(0.3f, 0.3f, 0.3f);
         for (const auto pt : x) {
-            glVertex2f(static_cast<GLfloat>(v[pt][0]), static_cast<GLfloat>(v[pt][1]));
+            cam.vec2(static_cast<GLfloat>(v[pt][0]), static_cast<GLfloat>(v[pt][1]));
         }
         glEnd();
     };
@@ -100,18 +170,18 @@ class DrawableScanLineFill : public DrawablePolygon {
             scale = (float)(dx / dy);
             offset = (float)((y1 * x2 - x1 * y2) / dy);
         }
-        inline float intersection(float scanline) {
-            // horizontal scanline intersection from the left
+        inline double intersection(double scanline) const {
+            // horizontal scan-line intersection from the left
+            // double precision for the calculation is required to avoid artifacts
             return scanline * scale + offset;
         }
     };
     mapbox::fixtures::FixtureTester* shape;
-    std::vector<float> intersections; /* contains current sorted intersections */
+    std::vector<Edge*> activeList; /* contains current sorted intersections */
     std::vector<std::vector<Edge>> edgeTables; /* contains all edges sorted by yMin */
 public:
     explicit DrawableScanLineFill(mapbox::fixtures::FixtureTester* fixture) : shape(fixture) {
         auto& polygon = shape->polygon();
-        glLineWidth(float(fbHeight) / height);
         edgeTables.reserve(polygon.size());
         for (const auto &ring : polygon) {
             std::vector<Edge> edgeTable;
@@ -137,19 +207,22 @@ public:
         }
         Edge* edgeList = edgeTable.empty() ? nullptr : &edgeTable[0];
 
-        // halfWidth used to center lines on pixels
         const double halfWidth = lineWidth * 0.5;
-        top -= halfWidth;
-        bottom += halfWidth;
+        top -= lineWidth;
+        bottom += lineWidth;
 
-        double y = top;
-        while(y < bottom && edgeList) {
-            intersections.clear();
+        double y0 = top;
+        while(y0 < bottom && edgeList) {
+            double y1 = y0 + lineWidth;
+            if (y1 == y0) { y1 = std::nextafter(y1, bottom); }
+            const double y = y0 + halfWidth;
+
+            activeList.clear();
             Edge* prevEdge = nullptr;
             for (auto edge = edgeList; edge != nullptr; edge = edge->next) {
                 const auto min = edge->yMin, max = edge->yMax;
                 if (min <= y && y < max) {
-                    intersections.push_back(edge->intersection(static_cast<float>(y)));
+                    activeList.push_back(edge);
                 }
                 if (min > y)  {
                     break;
@@ -161,37 +234,52 @@ public:
                     prevEdge = edge;
                 }
             }
-            std::sort(intersections.begin(), intersections.end());
+            std::sort(activeList.begin(), activeList.end(), [&](Edge *a, Edge *b) {
+                return a->intersection(y) < b->intersection(y);
+            });
 
-            auto x1 = .0f;
-            for (std::size_t i = 0; i < intersections.size(); i++) {
-                auto x2 = intersections[i];
+            double x1y0 = 0, x1y1 = 0;
+            for (std::size_t i = 0; i < activeList.size(); i++) {
+                Edge *edge = activeList[i];
+
+                // use slope to make MSAA possible
+                const double x2y0 = edge->intersection(std::max<double>(edge->yMin, y0));
+                const double x2y1 = edge->intersection(std::min<double>(edge->yMax, y1));
                 if ((i % 2) != 0) {
-                    glVertex2f(static_cast<GLfloat>(x1 + halfWidth), static_cast<GLfloat>(y));
-                    glVertex2f(static_cast<GLfloat>(x2 + halfWidth), static_cast<GLfloat>(y));
+                    cam.vec2(x1y0, y0);
+                    cam.vec2(x1y1, y1);
+                    cam.vec2(x2y1, y1);
+                    cam.vec2(x2y0, y0);
                 }
-                x1 = x2;
+                x1y0 = x2y0;
+                x1y1 = x2y1;
             }
 
-            y += lineWidth;
+            y0 = y1;
         }
     }
     void drawFill() override {
         for (std::size_t i = 0; i < edgeTables.size(); i++) {
             auto& edgeTable = edgeTables[i];
-            glBegin(GL_LINES);
+            glBegin(GL_QUADS);
             if (i == 0) {
                 glColor3f(0.3f, 0.3f, 0.3f);
             } else {
                 glColor3f(1, 1, 1);
             }
-            scanLineFill(edgeTable, top, bottom, (bottom - top) / fbHeight);
+            double x0 = 1;
+            double y0 = 1;
+            cam.toWorld(&x0, &y0);
+            double x1 = -1;
+            double y1 = -1;
+            cam.toWorld(&x1, &y1);
+            scanLineFill(edgeTable, y0, y1, (y1 - y0) / cam.viewHeight);
             glEnd();
         }
     }
     void drawMesh() override {
         auto& polygon = shape->polygon();
-        glLineWidth(float(fbHeight) / height);
+        glLineWidth(float(cam.viewHeight) / height);
         for (std::size_t i = 0; i < polygon.size(); i++) {
             auto& ring = polygon[i];
             glBegin(GL_LINES);
@@ -203,8 +291,8 @@ public:
             for (std::size_t j = 0; j < ring.size(); j++) {
                 auto& p0 = ring[j];
                 auto& p1 = ring[(j+1) % ring.size()];
-                glVertex2f(static_cast<GLfloat>(std::get<0>(p0)), static_cast<GLfloat>(std::get<1>(p0)));
-                glVertex2f(static_cast<GLfloat>(std::get<0>(p1)), static_cast<GLfloat>(std::get<1>(p1)));
+                cam.vec2(std::get<0>(p0), std::get<1>(p0));
+                cam.vec2(std::get<0>(p1), std::get<1>(p1));
             }
             glEnd();
         }
@@ -224,26 +312,6 @@ std::unique_ptr<DrawablePolygon> DrawablePolygon::makeDrawable(std::size_t index
 
 static std::array<std::unique_ptr<DrawablePolygon>, 3> tessellators;
 
-void scaleViewport(double zoom) {
-    zoom = 1. - std::pow(1.1, zoom);
-    double dx = zoom * (right - left);
-    double dy = zoom * (bottom - top);
-    left -= dx;
-    right += dx;
-    top -= dy;
-    bottom += dy;
-    dirtyViewport = true;
-}
-
-void translateViewport(double x, double y) {
-    double dx = x / fbWidth * (right - left);
-    double dy = y / fbHeight * (bottom - top);
-    left += dx;
-    right += dx;
-    top += dy;
-    bottom += dy;
-    dirtyViewport = true;
-}
 
 mapbox::fixtures::FixtureTester *getFixture(std::size_t i) {
     auto& fixtures = mapbox::fixtures::FixtureTester::collection();
@@ -260,6 +328,7 @@ int main() {
     }
 
     glfwWindowHint(GLFW_RESIZABLE, 0);
+    glfwWindowHint(GLFW_SAMPLES, 4);
     window = glfwCreateWindow(width, height, "Tessellation", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
@@ -297,44 +366,43 @@ int main() {
             tessellator = (tessellator + tessellators.size() - 1) % tessellators.size();
             dirtyTessellator = true;
         } else if (key == GLFW_KEY_KP_ADD) {
-            scaleViewport(1.);
+            dirtyViewport |= cam.scale(1.);
         } else if (key == GLFW_KEY_KP_SUBTRACT) {
-            scaleViewport(-1.);
+            dirtyViewport |= cam.scale(-1.);
         } else if (key == GLFW_KEY_R) {
-            dirtyViewport = dirtyShape = true;
+            dirtyTessellator = dirtyViewport = dirtyShape = true;
         } else if (key == GLFW_KEY_W) {
-            translateViewport(.0, -fbWidth / 50.);
+            dirtyViewport |= cam.move(.0, cam.viewHeight / 50.);
         } else if (key == GLFW_KEY_A) {
-            translateViewport(-fbWidth / 50., .0);
+            dirtyViewport |= cam.move(cam.viewWidth / 50., .0);
         } else if (key == GLFW_KEY_S) {
-            translateViewport(.0, fbWidth / 50.);
+            dirtyViewport |= cam.move(.0, -cam.viewHeight / 50.);
         } else if (key == GLFW_KEY_D) {
-            translateViewport(fbWidth / 50., .0);
+            dirtyViewport |= cam.move(-cam.viewWidth / 50., .0);
         }
     });
 
     glfwSetScrollCallback(window, [](GLFWwindow* /* window */, double /* xoffset */, double yoffset) {
-        scaleViewport(yoffset);
+        dirtyViewport |= cam.scale(yoffset);
     });
 
     glfwSetMouseButtonCallback(window, [](GLFWwindow* /* window */, int button, int action, int /* mods */){
         if (button == GLFW_MOUSE_BUTTON_LEFT) {
-            if(!mouseDrag && action == GLFW_PRESS) {
-                mouseDrag = true;
-            } else if(mouseDrag && action == GLFW_RELEASE) {
-                mouseDrag = false;
-            }
+            mouseDrag = action != GLFW_RELEASE;
         }
     });
 
     glfwSetFramebufferSizeCallback(window, [](GLFWwindow * /*win*/, int w, int h) {
-        fbWidth = w;
-        fbHeight = h;
+        cam.setView(w, h);
     });
 
+    int fbWidth, fbHeight;
     glfwGetFramebufferSize(window, &fbWidth, &fbHeight);
+    cam.setView(fbWidth, fbHeight);
 
     glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
 
     glClearColor(1, 1, 1, 1);
 
@@ -344,7 +412,7 @@ int main() {
         glfwGetCursorPos(window, &mouseX, &mouseY);
         double mouseDeltaX = mouseX - mousePrevX, mouseDeltaY = mouseY - mousePrevY;
         if (mouseDrag) {
-            translateViewport(-mouseDeltaX, -mouseDeltaY);
+            dirtyViewport |= cam.move(mouseDeltaX, mouseDeltaY);
         }
 
         if (dirtyShape) {
@@ -359,10 +427,10 @@ int main() {
             auto maxY = std::numeric_limits<double>::min();
             if (!polygon.empty()) {
                 for (const auto &pt : polygon[0]) {
-                    if (std::get<0>(pt) < minX) minX = std::get<0>(pt);
-                    if (std::get<1>(pt) < minY) minY = std::get<1>(pt);
-                    if (std::get<0>(pt) > maxX) maxX = std::get<0>(pt);
-                    if (std::get<1>(pt) > maxY) maxY = std::get<1>(pt);
+                    minX = std::min<double>(minX, std::get<0>(pt));
+                    minY = std::min<double>(minY, std::get<1>(pt));
+                    maxX = std::max<double>(maxX, std::get<0>(pt));
+                    maxY = std::max<double>(maxY, std::get<1>(pt));
                 }
             }
             const auto dimX = minX < maxX ? maxX - minX : 0;
@@ -370,23 +438,16 @@ int main() {
 
             auto midX = minX + dimX / 2;
             auto midY = minY + dimY / 2;
-            auto ext = 1.10 * std::max<double>(dimX, dimY) / 2;
+            auto ext = std::max<double>(dimX, dimY) / 2;
 
-            left = midX - ext;
-            right = midX + ext;
-            bottom = midY + ext;
-            top = midY - ext;
+            cam.setDefaults();
+            cam.limits(midX - ext, midX + ext, midY + ext, midY - ext);
         }
 
         if (dirtyViewport || dirtyShape || dirtyTessellator) {
-            glViewport(0, 0, fbWidth, fbHeight);
-            glMatrixMode(GL_PROJECTION);
-            glLoadIdentity();
-
-            glOrtho(left, right, bottom, top, 0, 1);
-            glMatrixMode(GL_MODELVIEW);
-
             glClear(GL_COLOR_BUFFER_BIT);
+
+            cam.apply();
 
             auto& drawable = tessellators[tessellator];
 
@@ -402,6 +463,7 @@ int main() {
             if (!drawMesh && !drawFill) {
                 drawMesh = drawFill = true;
             }
+
             if (drawFill) {
                 drawable->drawFill();
             }
@@ -409,6 +471,7 @@ int main() {
                 drawable->drawMesh();
             }
 
+            glFlush(); /* required for Mesa 3D driver */
             glfwSwapBuffers(window);
         }
 
